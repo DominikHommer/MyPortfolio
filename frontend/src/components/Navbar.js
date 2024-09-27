709cc62729feaf3cf06259b28edcc803d7f7cf69
DominikHommer <dominik.hommer@web.de>
2024-09-27 23:21:03 +0200

    created responsive design

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,24 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-scroll';
 import './Navbar.css';
 
 const Navbar = () => {
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    const closeMenu = () => {
+        setIsOpen(false); // Schließt das Menü nach Klick
+    };
+
     return (
         <nav className="navbar">
-            <ul>
-                <li><Link to="about" smooth={true} duration={500} spy={true} offset={0} activeClass="active">About</Link></li>
-                <li><Link to="skills" smooth={true} duration={500} spy={true} offset={0} activeClass="active">Skills</Link></li>
-                <li><Link to="workexp" smooth={true} duration={500} spy={true} offset={0} activeClass="active">Work Experience</Link></li>
-                <li><Link to="education" smooth={true} duration={500} spy={true} offset={0} activeClass="active">Education</Link></li>
-                <li><Link to="projects" smooth={true} duration={500} spy={true} offset={0} activeClass="active">Projects</Link></li>
-                <li><Link to="tictactoe" smooth={true} duration={500} spy={true} offset={0} activeClass="active">TicTacToe</Link></li>
-                <li><Link to="contact" smooth={true} duration={500} spy={true} offset={0} activeClass="active">Contact</Link></li>
+            <div className="hamburger" onClick={toggleMenu}>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <ul className={isOpen ? "show" : ""}>
+                <li><Link to="about" smooth={true} duration={500} spy={true} offset={0} activeClass="active" onClick={closeMenu}>About</Link></li>
+                <li><Link to="skills" smooth={true} duration={500} spy={true} offset={0} activeClass="active" onClick={closeMenu}>Skills</Link></li>
+                <li><Link to="workexp" smooth={true} duration={500} spy={true} offset={0} activeClass="active" onClick={closeMenu}>Work Experience</Link></li>
+                <li><Link to="education" smooth={true} duration={500} spy={true} offset={0} activeClass="active" onClick={closeMenu}>Education</Link></li>
+                <li><Link to="projects" smooth={true} duration={500} spy={true} offset={0} activeClass="active" onClick={closeMenu}>Projects</Link></li>
+                <li><Link to="tictactoe" smooth={true} duration={500} spy={true} offset={0} activeClass="active" onClick={closeMenu}>TicTacToe</Link></li>
+                <li><Link to="contact" smooth={true} duration={500} spy={true} offset={0} activeClass="active" onClick={closeMenu}>Contact</Link></li>
             </ul>
         </nav>
     );
 };
 
 export default Navbar;
+
+
 
 
 
